93eb22d3da8219a41d6d3a096df82f2ecb8844a3
Harsh Tiwari <131146410+Master-harsh@users.noreply.github.com>
2024-09-11 14:03:37 +0000

    Datesin

---
+++ 01_basics/07_Datesin.js
@@ -0,0 +1,32 @@
+//DATEs
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+//let myCreatedDate = new Date(2023, 0, 23);
+
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2024-01-14")
+let myCreatedDate = new Date("11-09-2024")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);// it used when we want to poll for any thing 
+// console.log(myCreatedDate.getTime()); // when we book hotel then we need to compare dates
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+
+console.log(newDate.getDay());
+console.log(newDate.getMonth());// here remember one thing it always starts with 0-11 months
+
+newDate.toLocaleString('default', {
+    weekday: 
+})
+
+
