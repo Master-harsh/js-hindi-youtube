652b6a429c324d5419b657be501971daeb9e5d19
Harsh Tiwari <131146410+Master-harsh@users.noreply.github.com>
2024-09-11 10:21:54 +0000

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,34 @@
+const name = "Harsh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//const gamename = new String('Harshht')
+const gamename = new String('Harsh-ht')
+
+
+// 1
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('t'));
+//console.log(gamename.indexOf('h'));
+
+ const newString = gamename.substring(0, 4)
+// console.log(newString);
+// console.log(gamename.length)
+
+// const anotherString = gamename.slice(-7, 4)
+// console.log(anotherString);
+
+const url = "https://hitesh.com/hitesh%20chaudhary"
+console.log(url.replace('%20', '_'));
+
+console.log(url.includes('sunder'));
+
+
+
+
