8947c976ff7353d23f1ec34f527fd0b12db9657c
Harsh Tiwari <131146410+Master-harsh@users.noreply.github.com>
2024-09-12 11:05:08 +0000

    Arrays part1

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,45 @@
+// ARRAYS
+
+const myArr = [0, 1, 2, 3, 4, 5] // its element set according to you there is no restriction to store similer type of data in array
+const myHeros = ["Shaktiman", "Naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+
+// console.log(myArr[0]);
+
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift() // they also used to add or remove elements in array but in the front side of array
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9)); // it results in negative number what ever you ask which is not in array -1
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(typeof newArr);
+
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn2);
+console.log("C", myArr);
+
+
+
+
+//console.log(myArr);
